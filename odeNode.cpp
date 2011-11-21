a9c1e4198885a5d2fa00c8681b02c65cd9b0ae4c
Alexander Rothkegel <Alex.Rothkegel@web.de>
2011-11-21 12:11:00 +0000

    adding missing file

---
+++ odeNode.cpp
@@ -0,0 +1,30 @@
+#include "odeNode.h"
+
+#include <gsl/gsl_errno.h>
+
+
+namespace conedy {
+
+
+
+
+
+	int odeNode::dgl ( double t,const double y[], double f[], void *params )
+	{
+		list<containerNode<baseType,1>*>::iterator it;
+		for ( it = nodeList.begin(); it != nodeList.end();it++ )
+			( * ( (odeNode *)*it )) ( &y[ ( *it )->startPosGslOdeNodeArray], &f[ ( *it )->startPosGslOdeNodeArray] );
+		return GSL_SUCCESS;
+	}
+
+
+
+	void odeNode::dgl (baseType *y, baseType* f)
+	{
+		list<containerNode<baseType,1>*>::iterator it;
+		for ( it = nodeList.begin(); it != nodeList.end();it++ )
+			( * ( (odeNode *)*it )) ( &y[ ( *it )->startPosGslOdeNodeArray], &f[ ( *it )->startPosGslOdeNodeArray] );
+	}
+
+
+}
