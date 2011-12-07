0297432e3d60ca93d35ea31c2d8d5c6eb2b02987
Alexander Rothkegel <Alex.Rothkegel@web.de>
2011-12-07 11:06:54 +0100

    updating test-scripts for the paper

--- testing/scriptsPaperDoku/paper_example1.py
+++ testing/scriptsPaperDoku/paper_example1.py
@@ -1,8 +1,10 @@
 import conedy as co
 net = co.network()
-bark = co.barkley()
+co.set ("gaussianBarkley_sigma",  0.18)
+
+bark = co.gaussianBarkley()
 bark.setState (0.0, 0.0)
-net.lattice(512, 512, 1.0, bark)
+net.lattice(512, 512, 1.0, bark, co.staticWeightedEdge (3.84))
 net.rewire(0.001)
 net.evolve(0.0, 20.0)
 net.observeAll("waves.dat", co.component(0))
