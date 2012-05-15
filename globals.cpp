9fea6b58b9cd1b59ef25eb0818173a26b60d78c0
Alexander Rothkegel <Alex.Rothkegel@web.de>
2012-05-15 10:06:30 +0200

    globals.cpp was missing

---
+++ globals.cpp
@@ -0,0 +1,11 @@
+#include "globals.h"
+
+
+
+template <> int typeInteger <int>() { return 0;}
+template <> int typeInteger <double>() { return 1;}
+template <> int typeInteger <string>() { return 2;}
+template <> int typeInteger <bool>() { return 3;}
+
+map <string, void*> globals::value;
+map <string, int> globals::type;
