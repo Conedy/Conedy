9770bb88708bf1a430294bd1daff56f3ed91e5bd
Alexander Rothkegel <Alex.Rothkegel@web.de>
2012-06-05 09:53:58 +0200

    included old version of gslOdeNode for allow compilation with gsl1.14

--- gslOdeNode.cpp
+++ gslOdeNode.cpp
@@ -2,8 +2,126 @@
 #include "gslOdeNode.h"
 
 
+
 #ifdef DOUBLE
 
+#if GSL_MINOR_VERSION < 15
+
+
+namespace conedy
+{
+
+
+	params<string> * gslOdeNode::gslStepType;
+
+	params<baseType> *gslOdeNode::gslErrors;
+
+
+	double * gslOdeNode::errors;
+
+
+
+	double gslOdeNode::stepSize = 0.001;
+
+
+	void gslOdeNode::evolve(double timeTilEvent)
+	{
+
+
+		baseType startTime = dynNode::time;
+		baseType endTime = dynNode::time + timeTilEvent;
+
+
+		if (error_abs() == 0.0 && error_rel() == 0.0)
+		{									// without stepsize control
+
+			throw "fixed stepsize seems to be broken at the moment.";
+
+			int stepCount = timeTilEvent/stepSize + 1.0 - 1e-8 ;
+			double dt = timeTilEvent / stepCount;
+
+
+				if ( gsl_odeiv_step_apply ( gslStep,dynNode::time,
+							timeTilEvent,
+							containerNode<baseType,3>::dynamicVariablesOfAllDynNodes,
+							errors,
+							NULL,
+							NULL,
+							&gslOdeSys) !=GSL_SUCCESS )
+					throw "gslError!";
+
+
+//			for (int i = 0; i < stepCount; i++)
+//			{
+//				if ( gsl_odeiv_step_apply ( gslStep,dynNode::time,
+//							dt,
+//							containerNode<baseType,3>::dynamicVariablesOfAllDynNodes,
+//							errors,
+//							NULL,
+//							NULL,
+//							&gslOdeSys) !=GSL_SUCCESS )
+//					throw "gslError!";
+////				dynNode::time += dt;
+//#ifdef DEBUG
+//								cout << dt << "\n";
+//#endif
+//			}
+//
+
+
+		}
+		else
+		{									// with stepsize control
+			while(dynNode::time < endTime)
+			{
+				int status = gsl_odeiv_evolve_apply (gslEvolve,
+						gslControl,
+						gslStep,
+						&gslOdeSys,
+						&dynNode::time,
+						endTime,
+						&stepSize,
+						containerNode<baseType,3>::dynamicVariablesOfAllDynNodes) ;
+				if (status != GSL_SUCCESS)
+					break;
+
+#ifdef DEBUG
+								cout << stepSize << "\n";
+#endif
+			}
+		}
+
+		dynNode::time = startTime;    // changing the time is handled by the evolve-loop in dynNetwork.cpp
+
+
+
+	}
+
+	const gsl_odeiv_step_type* gslOdeNode::stepType = NULL;
+
+	//! Zeiger auf den Speicher für die Stufenfunktion
+
+	gsl_odeiv_step *gslOdeNode::gslStep;
+
+	//! Zeiger auf die Kontrollfunktion (überprüft Fehler der Stufenfunktion)
+
+	gsl_odeiv_control *gslOdeNode::gslControl;
+
+	//! Zeiger auf die Evolutionsfuktion (diese vereint die Stufenfunktion mit der Kontrollfunktion)
+
+	gsl_odeiv_evolve *gslOdeNode::gslEvolve;
+
+	//! Datentyp für das ODE-System
+
+	gsl_odeiv_system gslOdeNode::gslOdeSys;
+
+
+}
+
+
+
+
+#else
 
 
 namespace conedy
@@ -75,5 +193,5 @@ namespace conedy
 	bool gslOdeNode::alreadyInitialized = false;
 
 }
-
+#endif
 #endif
