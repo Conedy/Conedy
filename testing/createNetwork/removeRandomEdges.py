c51e43e962e991e0d3669f5be3ce15cf8ae1ac8f
Sara Tutar <sara@angelique.(none)>
2013-03-06 15:12:20 +0100

    Added the documentation for the new functions removeRandomEdge() and removeEdge()

---
+++ testing/createNetwork/removeRandomEdges.py
@@ -0,0 +1,26 @@
+import conedy as co
+
+nodeTemplate=co.pcoIntegrateFireDelay()
+
+N=co.network()
+
+N.cycle(100, 4, nodeTemplate, co.weightedEdge(0.1))
+N.rewireUndirected(0.1)
+
+N.observePhaseCoherence("meanPhase0")
+N.evolve(0.0, 100.0)
+N.removeObserver()
+
+N.observePhaseCoherence("meanPhase1")
+N.evolve(100.0, 200.0)
+N.removeObserver()
+
+N.removeRandomEdges(0.1, co.weightedEdge(0.1))
+
+N.observePhaseCoherence("NewMeanPhase")
+N.evolve(100.0, 200.0)
+
+
+
+
+
