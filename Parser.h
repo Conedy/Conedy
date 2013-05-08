82e19ea22a1a1fe38ee5932374287e7e7b83e25c
Alexander Rothkegel <Alex.Rothkegel@web.de>
2013-05-08 16:14:08 +0200

    compatibility patch for bisonc++4.03

--- Parser.h
+++ Parser.h
@@ -33,6 +33,7 @@ class Parser: public ParserBase
         int lookup(bool recovery);
         void nextToken();
 		  void print__();
+ 		  void exceptionHandler__(std::exception const &exc);
 };
 
 
