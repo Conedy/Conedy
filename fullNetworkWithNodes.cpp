6379005518a969fc7f8a6f5c9ad32827cb735cce
Alexander Rothkegel <alex.rothkegel@web.de>
2013-12-19 17:17:48 -0800

    i

---
+++ fullNetworkWithNodes.cpp
@@ -0,0 +1,38 @@
+
+
+#include "fullNetworkWithNodes.h"
+
+void registerStandards()
+{
+
+	stdSdeIntegrator::registerStandardValues();
+	pcoBase::registerStandardValues();
+	containerNode<baseType, 0>::registerStandardValues();
+	dynNetwork::registerStandardValues();
+//	createNetwork::registerStandardValues();
+	escapeNoiseNeuron::registerStandardValues();
+#ifdef DOUBLE
+	gslOdeNode::registerStandardValues();
+#endif
+
+#ifdef LDOUBLE
+	stdOdeIntegrator::registerStandardValues();
+#endif
+
+	network::registerStandardValues();
+	pcoDelay::registerStandardValues();
+//	pulseCoupledExcitatoryNeuron::registerStandardValues();
+//	sigEdgeParams::registerStandardValues();
+	streamInNode::registerStandardValues();
+	streamOutNode::registerStandardValues();
+	streamOutNodeHist::registerStandardValues();
+	sdeNode::registerStandardValues();
+#if OPENGL
+	GLNetwork<baseType>::registerStandardValues();
+#endif
+	#include "generatedRegisterStandards.h"
+
+}
+
+
+
