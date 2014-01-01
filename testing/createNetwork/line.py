d3b2220c2b8c99b097f2a56d232c82a1f3329fd0
Alexander Rothkegel <alexander@rothkegel.de>
2014-01-01 17:56:33 +0100

    commi

--- testing/createNetwork/line.py
+++ testing/createNetwork/line.py
@@ -8,7 +8,7 @@ N = co.network()
 N.line(1000,1, co.node(), co.weightedEdge(1.0))  # Creates a closed chain of 100 nodes where each is connected to its 10 nearest neighbors to each side.
 
 
-print "should be 1000:" + str ( N.size() )
+print "should be 1000:" + str ( N.numberVertices() )
 print "should be 1.0:" + str ( N.meanWeight() )
 
 print "should be 0.0:" + str ( N.meanClustering() ) 
